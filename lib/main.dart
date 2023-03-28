08b46087d6a8a0783a9823af21d468a6f1634621
UlugbekYarkinov <yarkinovulugbek@gmail.com>
2023-03-28 16:43:06 +0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:chatter_test/screens/welcome_screen.dart';
+
+void main() => runApp(const FlashChat());
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
